66faa0bfe8e7e109c13d2bae8f51e30fef3837b3
anaclarabck <ackyotoku@gmail.com>
2021-05-03 15:45:49 -0300

    [ADD] funçao de sortio

---
+++ bloco-8-higher-order-functions-javascript-es6/higher-order-functions-do-javascript-es6_1/sorteio.js
@@ -0,0 +1,7 @@
+// Desenvolva uma HOF que retorna o resultado de um sorteio. Esta HOF irá gerar um número aleatório entre 1 e 5 recebendo como parâmetros o número apostado e uma função que checa se o número apostado é igual ao número sorteado. O retorno da sua HOF deve ser uma string (Ex: "Tente novamente" ou "Parabéns você ganhou"). 
+
+const resultDraw = (betNumber, checkDraw) => checkDraw(betNumber) ? `Parabéns` : `Tente novamente`;
+
+const checkDraw = (number) => (number === Math.ceil(Math.random() *  5));
+
+console.log(resultDraw(3, checkDraw))
