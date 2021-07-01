e41ed3e3191a8c302cad440866d7631277c2abe4
anaclarabck <ackyotoku@gmail.com>
2021-07-01 20:10:26 -0300

    Exercícios ciclo de vida de componentes e react router

---
+++ bloco-13-ciclo-de-vida-de-componentes-e-react-router/ciclo-de-vida-de-componentes-e-react-router_1/exemplo1/src/Counter.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0
+    };
+    console.log("construtor");
+  }
+
+  componentDidMount() {
+    console.log("componentDidMount");
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log("shouldComponentUpdate");
+    return true;
+  }
+// Note que, durante o processo de atualização, o método render é chamado novamente. Isso acontece porque, quando se atualiza uma props ou estado, o React "pede" que o componente seja renderizado no DOM. Como apresentamos acima, caso seja válido, podemos impedir essa renderização retornando false com o método shouldComponentUpdate . 
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log("componentDidUpdate");
+  }
+
+  render() {
+    console.log("render");
+    return (
+      <div>
+        <p>Contador</p>
+        <button
+          onClick={() => this.setState((state) => ({ counter: state.counter + 1 }))}
+        >
+          Soma
+        </button>
+        <p>{this.state.counter}</p>
+      </div>
+    );
+  }
+}
+
+export default Counter;
+
+// Perceba que o estado só é de fato atualizado quando chega no método componentDidUpdate . Por isso, caso seja necessário impedir uma renderização, você deve utilizar o método shouldComponentUpdate , que permite comparar os atuais e próximos estados ou props e adicionar a lógica. 
